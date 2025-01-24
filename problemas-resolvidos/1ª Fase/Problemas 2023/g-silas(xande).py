26329f506eac2b9ccd1230fb829c002f29acdd74
Alexandre Alcindo <xandoliveira2@users.noreply.github.com>
2025-01-24 20:24:19 -0300

    resolvendo pequeno problema e removendo comentarios

--- problemas-resolvidos/1ª Fase/Problemas 2023/g-silas(xande).py
+++ problemas-resolvidos/1ª Fase/Problemas 2023/g-silas(xande).py
@@ -1,38 +1,35 @@
 from time import sleep
 
 caminhos = dict()
-
 ja_visitados = []
 forca = int(input())
 matriz = []
 linhas,colunas = list(map(int,input().split(' ')))
+
 for inputLinhas in range(linhas):
     matriz.append(input().split(' '))
-
-posicaoInicial = (0,0)
+    
+posicaoInicial = ()
 for linha in range(len(matriz)):
     for coluna in range(len(matriz[0])):
         if matriz[linha][coluna] == 'S':
-            posicao = (linha,coluna)
+            posicaoInicial = (linha,coluna)
         elif matriz[linha][coluna].isnumeric():
             if int(matriz[linha][coluna]) <= forca:
                 matriz[linha][coluna] = '.'
             else:
                 matriz[linha][coluna] = '#'
-
-
+                
 proximos_na_fila = []
 proximos_na_fila.append(posicaoInicial)
 caminhos[posicaoInicial] = None
-
-
 Objetivo = tuple()
 for linha in range(len(matriz)):
     for coluna in range(len(matriz[0])):
         if matriz[linha][coluna] == 'K':
             Objetivo = (linha,coluna)
             break
-
+        
 def adicionarVizinhosNaFila(posicaoAtual : tuple):
     for proximos_x in [-1,0,1]:
         for proximos_y in [-1,0,1]:
@@ -40,15 +37,10 @@ def adicionarVizinhosNaFila(posicaoAtual : tuple):
                 if (posicaoAtual[0] + proximos_x, posicaoAtual[1] + proximos_y) not in ja_visitados and (posicaoAtual[0] + proximos_x, posicaoAtual[1] + proximos_y) not in proximos_na_fila and matriz[posicaoAtual[0] + proximos_x][posicaoAtual[1] + proximos_y] != "#" and abs(proximos_x) != abs(proximos_y) :
                     proximos_na_fila.append((posicaoAtual[0] + proximos_x, posicaoAtual[1] + proximos_y))
                     caminhos[(posicaoAtual[0] + proximos_x, posicaoAtual[1] + proximos_y)] = posicaoAtual
-
-
+                    
 printou = False
 for item in proximos_na_fila:
-
     matriz[item[0]][item[1]] = 'S'
-
-    # for linha in matriz:
-    #     print(linha)
     if item == Objetivo:
         resposta = []
         while True:
@@ -59,10 +51,7 @@ for item in proximos_na_fila:
                 item =  caminhos[item]
         print(len(resposta))
         printou = True
-        # print("Chegou =D")
-        # print(caminhos)
         break
     adicionarVizinhosNaFila(item)
-    # sleep(1)
 if not printou: # uso apenas para focar no nosso boca, se não poderia colocar return encima e deixar o print em aberto em baixo
     print("N")
