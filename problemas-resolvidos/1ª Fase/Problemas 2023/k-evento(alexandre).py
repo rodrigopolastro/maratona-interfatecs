a0f0d9b1e994e5a49a09d19196b4bbf0bf1b9656
Alexandre Alcindo <xandoliveira2@users.noreply.github.com>
2025-01-24 20:10:55 -0300

    adicionando g-silas (xande style kk )

--- problemas-resolvidos/1ª Fase/Problemas 2023/k-evento(alexandre).py
+++ problemas-resolvidos/1ª Fase/Problemas 2023/k-evento(alexandre).py
@@ -1,3 +1,4 @@
+# inicio 8:25 -> término 21:34
 while True:
     pontos , ruas =list(map(int,input().split()))
     if pontos == 0 and ruas == 0:
@@ -11,13 +12,9 @@ while True:
         caminhos[entrada[0]].append(entrada[1])
         if entrada[2] == 2:
             caminhos[entrada[1]].append(entrada[0])
-    
     qtdeWays = len(caminhos) - 1
-    
     semMovimento = 0
-
     while True:
-    
         if semMovimento == qtdeWays:
             break
         else:
@@ -26,6 +23,8 @@ while True:
                 for item in caminhos[ways]:                   
                     caminhos[ways].extend(caminhos[item])
                     caminhos[ways] = list(set(caminhos[ways]))
+                    if len(caminhos[ways]) == qtdeWays:
+                        break
                 depoisAdicao = len(list(set(caminhos[ways])))
                 if antesAdicao == depoisAdicao:
                     semMovimento +=1
@@ -33,26 +32,10 @@ while True:
                     semMovimento = 0
                 if semMovimento == qtdeWays:
                     break
-                    
-                    
-            # encaixouFor = False
-            # for listaResultado in range(1,len(caminhos)+1):
-            #     Checked = set()
-            #     # for item in caminhos[listaResultado]:
-            #     for caminhosNumero in range(1,pontos+1): 
-            #         if caminhosNumero in caminhos[listaResultado] and caminhosNumero not in Checked:
-            #             caminhos[listaResultado].add(caminhosNumero)
-            #             Checked.add(caminhosNumero)
-            #             encaixouFor = True
-            # if encaixouFor == False:
-            #     semMovimento +=1
-            # else:
-            #     semMovimento = 0
     printarN = False
     for item in range(1,len(caminhos)):
         if len(caminhos[item]) < pontos:
             printarN = True
-         
     if printarN:
         print("N")
     else:
