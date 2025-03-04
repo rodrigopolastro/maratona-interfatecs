6aebd5d3f5c4c430536caa51d8c5f65afbed5b02
Alexandre Alcindo <xandoliveira2@users.noreply.github.com>
2025-03-04 13:08:26 -0300

    preparando ambiente para simulado

--- problemas-resolvidos/1ª Fase/Problemas 2014/d-paint.py
+++ problemas-resolvidos/1ª Fase/Problemas 2014/d-paint.py
@@ -1,20 +1,21 @@
-
-def introduzirVizinhos(comparacao:str|int,posicao:tuple,matriz,jaVisitados : list):
+import sys
+sys.setrecursionlimit(1000000)
+def introduzirVizinhos(comparacao:str|int,posicao:tuple,matriz,jaVisitados : set):
     global resposta
     if 0 > posicao[0] or posicao[0] >= len(matriz) or 0 > posicao[1] or posicao[1] >= len(matriz[0]):
         return
     if matriz[posicao[0]][posicao[1]] != comparacao or posicao in jaVisitados:
         return
     else:
-        resposta.append(posicao)
-        jaVisitados.append(posicao)
+        resposta.add(posicao)
+        jaVisitados.add(posicao)
         introduzirVizinhos(comparacao,(posicao[0]+1,posicao[1]),matriz,jaVisitados)
         introduzirVizinhos(comparacao,(posicao[0]-1,posicao[1]),matriz,jaVisitados)
         introduzirVizinhos(comparacao,(posicao[0],posicao[1]+1),matriz,jaVisitados)
         introduzirVizinhos(comparacao,(posicao[0],posicao[1]-1),matriz,jaVisitados)
         
 while True:
-    jaVisitados = []
+    jaVisitados = set()
     x , y = [int(_) for _ in input().split()]
     if x == 0:
         break
@@ -23,7 +24,7 @@ while True:
         matriz.append(input().split())
     referencia = tuple([int(_)-1 for _ in input().split()])
     comparacao = matriz[referencia[0]][referencia[1]]
-    resposta = []
+    resposta = set()
     introduzirVizinhos(comparacao,referencia,matriz,jaVisitados)
     print(len(resposta))
     
