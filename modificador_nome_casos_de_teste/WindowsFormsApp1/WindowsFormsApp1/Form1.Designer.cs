c7471af16f9e7ec5c71e86b38ab559155547d2aa
Alexandre Alcindo <xandoliveira2@users.noreply.github.com>
2025-07-09 13:01:48 -0300

    fazendo o programa e alterando todos de 2019

---
+++ modificador_nome_casos_de_teste/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnConverter = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.txtCaminhoArquivo = new System.Windows.Forms.TextBox();
+            this.btnProcurarPasta = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnConverter
+            // 
+            this.btnConverter.BackColor = System.Drawing.Color.LawnGreen;
+            this.btnConverter.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConverter.Location = new System.Drawing.Point(294, 297);
+            this.btnConverter.Name = "btnConverter";
+            this.btnConverter.Size = new System.Drawing.Size(130, 47);
+            this.btnConverter.TabIndex = 0;
+            this.btnConverter.Text = "Converter";
+            this.btnConverter.UseVisualStyleBackColor = false;
+            this.btnConverter.Click += new System.EventHandler(this.btnConverter_Click);
+            // 
+            // txtCaminhoArquivo
+            // 
+            this.txtCaminhoArquivo.Location = new System.Drawing.Point(55, 152);
+            this.txtCaminhoArquivo.Name = "txtCaminhoArquivo";
+            this.txtCaminhoArquivo.Size = new System.Drawing.Size(654, 22);
+            this.txtCaminhoArquivo.TabIndex = 1;
+            // 
+            // btnProcurarPasta
+            // 
+            this.btnProcurarPasta.Location = new System.Drawing.Point(715, 152);
+            this.btnProcurarPasta.Name = "btnProcurarPasta";
+            this.btnProcurarPasta.Size = new System.Drawing.Size(36, 25);
+            this.btnProcurarPasta.TabIndex = 2;
+            this.btnProcurarPasta.Text = ". . .";
+            this.btnProcurarPasta.UseVisualStyleBackColor = true;
+            this.btnProcurarPasta.Click += new System.EventHandler(this.btnProcurarPasta_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnProcurarPasta);
+            this.Controls.Add(this.txtCaminhoArquivo);
+            this.Controls.Add(this.btnConverter);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConverter;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox txtCaminhoArquivo;
+        private System.Windows.Forms.Button btnProcurarPasta;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
+
---
+++ modificador_nome_casos_de_teste/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConverter_Click(object sender, EventArgs e)
+        {
+            string caminhoArquivo = txtCaminhoArquivo.Text;
+            errorProvider1.SetError(txtCaminhoArquivo, string.Empty);
+            if (string.IsNullOrWhiteSpace(caminhoArquivo))
+            {
+                errorProvider1.SetError(txtCaminhoArquivo, "Insira um caminho para arquivo");
+                return;
+            }
+            if (MessageBox.Show("Deseja refatorar os nomes dos arquivos (retirando todas letras, deixando apenas os números da pasta " + txtCaminhoArquivo + "?", "Confirmação", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                string[] arquivos = Directory.GetFiles(caminhoArquivo);
+                if (arquivos.Length == 0)
+                {
+                    MessageBox.Show("Não encontrado arquivos na pasta, verifique o caminho");
+                    return;
+                }
+                int arquivosModificados = 0;
+                foreach (var caminhoOriginal in arquivos)
+                {
+                    string nomeArquivo = Path.GetFileNameWithoutExtension(caminhoOriginal);
+                    string extensao = Path.GetExtension(caminhoOriginal);
+                    string pastaPai = Path.GetDirectoryName(caminhoOriginal);
+
+                    // Se o nome contém pelo menos um dígito
+                    if (Regex.IsMatch(nomeArquivo, @"\d"))
+                    {
+                        // Remove tudo que não for dígito
+                        string novoNome = new string(nomeArquivo.Where(char.IsDigit).ToArray()) + extensao;
+                        string caminhoNovo = Path.Combine(pastaPai, novoNome);
+
+                        if (string.Equals(caminhoNovo, caminhoOriginal, StringComparison.OrdinalIgnoreCase))
+                        {
+                            continue;
+                        }
+
+                        // Se o novo nome já existir, comparar o conteúdo
+                        if (File.Exists(caminhoNovo))
+                        {
+                            bool conteudoIgual = FileEquals(caminhoOriginal, caminhoNovo);
+                            if (conteudoIgual)
+                            {
+                                File.Delete(caminhoOriginal);
+                            }
+                        }
+                        else
+                        {
+                            File.Move(caminhoOriginal, caminhoNovo);
+                        }
+                        arquivosModificados++;
+                    }
+
+                }
+
+                if (arquivosModificados > 0)
+                {
+                    MessageBox.Show($"{arquivosModificados}/{arquivos.Length} arquivos foram modificados", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show($"Houve algum erro, verifique os arquivos", "Falha grave", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+            }
+
+        }
+
+        private bool FileEquals(string caminho1, string caminho2)
+        {
+            byte[] file1 = File.ReadAllBytes(caminho1);
+            byte[] file2 = File.ReadAllBytes(caminho2);
+            return file1.SequenceEqual(file2);
+        }
+        private void btnProcurarPasta_Click(object sender, EventArgs e)
+        {
+            using(var fd = new FolderBrowserDialog())
+            {
+                if(!string.IsNullOrWhiteSpace(txtCaminhoArquivo.Text) && Directory.Exists(txtCaminhoArquivo.Text))
+                {
+                    fd.SelectedPath = txtCaminhoArquivo.Text;
+                }
+                if(fd.ShowDialog() != DialogResult.Cancel)
+                {
+                    string caminhoArquivo = fd.SelectedPath;
+
+                    txtCaminhoArquivo.Text = caminhoArquivo;
+
+
+                }
+                
+            }
+        }
+    }
+}
