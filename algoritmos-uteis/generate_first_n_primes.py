970ba2759c96e2e8e0a1bb25833f463bd0dde249
Rodrigo Polastro <rodrigo.polastro03@gmail.com>
2025-08-01 22:05:21 -0300

    adiciona funcao para gerar primos até N

--- algoritmos-uteis/generate_first_n_primes.py
+++ algoritmos-uteis/generate_first_n_primes.py
@@ -1,10 +1,11 @@
 import math
 
-def generate_first_n_primes(n):
-    if n < 6:
-        limit = 15
+# retorna uma lista com os primeiros N primos informados
+def first_n_primes(numberOfPrimes):
+    if numberOfPrimes < 6:
+        limit = 15 
     else:
-        limit = int(n * (math.log(n) + math.log(math.log(n)))) + 10
+        limit = int(numberOfPrimes * (math.log(numberOfPrimes) + math.log(math.log(numberOfPrimes)))) + 10
 
     sieve = [True] * (limit + 1)
     sieve[0] = sieve[1] = False
@@ -17,4 +18,22 @@ def generate_first_n_primes(n):
     primes = [i for i, is_p in enumerate(sieve) if is_p]
     #OBS: se quiser retornar somente o enésimo primo, utilize:
     # -> return primes[n-1]
-    return primes[:n] 
+    return primes[:numberOfPrimes] 
+
+# retorna a lista de primos até o limite informado (INCLUI O LIMITE NO INTERVALO)
+def primes_up_to_n(limit):
+    sieve = [True] * (limit + 1)
+    sieve[0] = sieve[1] = False
+
+    for i in range(2, int(math.sqrt(limit)) + 1):
+        if sieve[i]:
+            for j in range(i * i, limit + 1, i):
+                sieve[j] = False
+
+    primes = [i for i, is_p in enumerate(sieve) if is_p]
+    return primes
+
+while True:
+    n = int(input())
+    print('primeiros ', n, ' primos: ', first_n_primes(n)[-1])
+    print('primos até ', n, ': ', primes_up_to_n(n)[-1])
