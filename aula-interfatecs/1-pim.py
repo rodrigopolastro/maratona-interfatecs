bc5b4264e4162b2dc7d140e35c41e298fb3ead10
gabrielalmir <gabr.almir@gmail.com>
2026-03-20 23:13:23 -0300

    feat: adicionar comentarios de 1-pim e 2-placas

--- aula-interfatecs/1-pim.py
+++ aula-interfatecs/1-pim.py
@@ -1,21 +1,33 @@
 # Exercício de J da 2ªfase de 2023
 
-numero = input() # explica o input()
-numero = int(numero) # explica pq tem que converter para int
+entrada = input() # explica o input()
+numero_maximo = int(entrada) # explica pq tem que converter para int
 
 resposta = ''
-lista_numeros = range(numero) # gera lista de números
+lista_numeros = range(numero_maximo) # gera lista de números
+
 for numero_atual in lista_numeros:
     numero_atual = numero_atual + 1
     resultado = numero_atual % 4
+
     if resultado == 0:
         resposta = resposta + 'pim' + ' '
     else:
         resposta = resposta + str(numero_atual) + ' '
+
 print(resposta.strip())
 
-# 1
+## Etapa 1: Onboarding
+### Vamos começar explicar começando como mostrar dados na tela e
+### obter dados do usuário com Python
 
-numero = input()
-numero = int(input())
+## Etapa 2: Baby-steps
+### Demonstrar passo a passo a construção em pegar esses dados
+### e mostrar o resultado final lógico (sem loop)
 
+## Etapa 3: Looping
+### Demonstrar a utilização de loop for (fixo), o funcionamento do range() e concatenar a resposta
+
+## Etapa 4: Pegadinha
+### Demonstrar esse código rodando sem o strip que é algumas pegadinhas típicas do boca
+### Demonstrar usando um caracter visível como "!" ou "😊"
--- aula-interfatecs/2-placas.py
+++ aula-interfatecs/2-placas.py
@@ -6,10 +6,16 @@ def nerdOuNao(string):
     for chr in string:
         if chr not in '10iIoO':
             return False
-    return True    
-    
+    return True
+
 for _ in range(n):
     if(nerdOuNao(input())):
         print("NERD!")
     else:
         print("Normal")
+
+## Etapa 1: revisão
+### Executar a lógica sem looping, funcionamento do "in"
+
+## Etapa 2: looping e função
+### Adicionar looping e facilidade de uso de uma definição da função
